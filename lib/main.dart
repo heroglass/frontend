e64c8db7d69ed2b343b70cc3b38611778bc0919e
jun <ggprgrkjh@naver.com>
2024-05-12 23:19:00 +0900

    feat(Payment): add iamport payment with kg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:iamport_flutter/iamport_payment.dart';
+import 'package:iamport_flutter/model/payment_data.dart';
+import 'package:tosspayments_widget_sdk_flutter/model/payment_info.dart';
+import 'package:tosspayments_widget_sdk_flutter/model/payment_widget_options.dart';
+import 'package:tosspayments_widget_sdk_flutter/payment_widget.dart';
+import 'package:tosspayments_widget_sdk_flutter/widgets/agreement.dart';
+import 'package:tosspayments_widget_sdk_flutter/widgets/payment_method.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +26,173 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: SplashScreen());
+  }
+}
+
+class PaymentStart extends StatefulWidget {
+  const PaymentStart({super.key, required Map<String, String> arguments});
+
+  @override
+  State<PaymentStart> createState() => _PaymentStartState();
+}
+
+class _PaymentStartState extends State<PaymentStart> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 393,
+          height: 852,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: Color(0xFF3F6541)),
+          child: Stack(
+            children: [
+              const Positioned(
+                  left: 98,
+                  top: 549,
+                  child: DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'Apple SD Gothic Neo',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                      child: Text(
+                        '국군장병 안경 신청 배송 서비스',
+                      ))),
+              const Positioned(
+                left: 153,
+                top: 480,
+                child: DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48,
+                      fontFamily: 'Apple SD Gothic Neo',
+                      fontWeight: FontWeight.w900,
+                      height: 0,
+                      letterSpacing: -4.80,
+                    ),
+                    child: Text(
+                      '히 글',
+                    )),
+              ),
+              Positioned(
+                left: 122,
+                top: 604,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(149, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    side: const BorderSide(width: 1, color: Colors.white),
+                    backgroundColor: const Color(0xFF3F6541),
+                  ),
+                  onPressed: () {
+                    // 버튼이 눌렸을 때 실행할 코드
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Payment()));
+                  },
+                  child: const Text("결제하기"),
+                ),
+              ),
+              Positioned(
+                left: 139,
+                top: 277,
+                child: Container(
+                  width: 115,
+                  height: 65,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/logo.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Payment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IamportPayment(
+      appBar: AppBar(
+        title: Text('아임포트 결제'),
+      ),
+      /* 웹뷰 로딩 컴포넌트 */
+      initialChild: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/iamport-logo.png'),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+              const Text('잠시만 기다려주세요...', style: TextStyle(fontSize: 20)),
+            ],
+          ),
+        ),
+      ),
+      /* [필수입력] 가맹점 식별코드 */
+      userCode: 'imp62043060',
+      /* [필수입력] 결제 데이터 */
+      data: PaymentData(
+          pg: 'html5_inicis',
+          // PG사
+          payMethod: 'card',
+          // 결제수단
+          name: '아임포트 결제데이터 분석',
+          // 주문명
+          merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}',
+          // 주문번호
+          amount: 100,
+          // 결제금액
+          buyerName: '홍길동',
+          // 구매자 이름
+          buyerTel: '01012345678',
+          // 구매자 연락처
+          buyerEmail: 'example@naver.com',
+          // 구매자 이메일
+          buyerAddr: '서울시 강남구 신사동 661-16',
+          // 구매자 주소
+          buyerPostcode: '06018',
+          // 구매자 우편번호
+          appScheme: 'example',
+          // 앱 URL scheme
+          cardQuota: [2, 3] //결제창 UI 내 할부개월수 제한
+          ),
+      /* [필수입력] 콜백 함수 */
+      callback: (Map<String, String> result) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => PaymentStart(arguments: result)),
+        );
+        print(result);
+
+        Navigator.pushReplacementNamed(
+          context,
+          '/result',
+          arguments: result,
+        );
+      },
+    );
   }
 }
 
@@ -374,8 +548,9 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) =>
-                const MyHomePage(title: "my first navigation")),
+            builder: (context) => const PaymentStart(
+                  arguments: {},
+                )),
       );
     });
   }
